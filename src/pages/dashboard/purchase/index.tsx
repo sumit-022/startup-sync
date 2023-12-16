de5296057f410cbe75ed82a638eab54a63845a1a
sumit-022 <lcb2021016@iiitl.ac.in>
2023-12-16 19:38:19 +0530

    changes

--- src/pages/dashboard/purchase/index.tsx
+++ src/pages/dashboard/purchase/index.tsx
@@ -2,19 +2,12 @@ import DashboardLayout from "@/components/layout";
 import { MdAdd } from "react-icons/md";
 import { CgMediaLive } from "react-icons/cg";
 import { RiAlarmWarningFill } from "react-icons/ri";
-import { BiHistory } from "react-icons/bi";
-import { TiCancel } from "react-icons/ti";
-import Button from "@/components/atoms/button";
-import { tableData } from "@/data/dashboard";
-import Table from "@/components/atoms/table";
-import Modal from "@/components/atoms/modal";
-import { modalAtom } from "@/atoms/modal.atom";
-import { useAtom } from "jotai";
-import JobOrderForm from "../../../components/common/joborder/joborder-form";
+import { useAuth } from "@/context/AuthContext";
+
 export default function Home() {
-  const [showModal, setShowModal] = useAtom(modalAtom);
+  const { authData } = useAuth();
   return (
-    <DashboardLayout>
+    <DashboardLayout user={authData}>
       <h1>Purchase Dashboard</h1>
     </DashboardLayout>
   );
--- src/pages/vendor/index.tsx
+++ src/pages/vendor/index.tsx
@@ -1,12 +1,14 @@
-import DashboardLayout from '@/components/layout'
-import React from 'react'
+import DashboardLayout from "@/components/layout";
+import React from "react";
+import { useAuth } from "@/context/AuthContext";
 
 const VendorPage = () => {
+  const { authData } = useAuth();
   return (
-      <DashboardLayout header sidebar>
-          <h1>Hello</h1>
+    <DashboardLayout header sidebar user={authData}>
+      <h1>Hello</h1>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default VendorPage
+export default VendorPage;
