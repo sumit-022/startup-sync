96888fa796221aca6bf5572e366984da208e364e
sumit-022 <lcb2021016@iiitl.ac.in>
2023-12-26 16:03:33 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import DashboardLayout from '@/components/layout'
+import React from "react";
+import DashboardLayout from "@/components/layout";
 
 const HomePage = () => {
   return (
     <DashboardLayout header sidebar>
-    <h1>Go to Purchase Dashboard</h1>
-  </DashboardLayout>;
-  )
-}
+      <h1>Go to Purchase Dashboard</h1>
+    </DashboardLayout>
+  );
+};
 
-export default HomePage
+export default HomePage;
