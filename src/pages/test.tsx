2dfe4703d2f9387be912fe8b072ab352e274c3a9
chakri68 <chakridevireddy69@gmail.com>
2024-02-13 03:12:00 +0530

    Added better types

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -13,11 +13,80 @@ export default function Test() {
     "Remark",
   ] as const;
 
+  const spares = [
+    {
+      name: "Spare 1",
+      "Order Qty": "2",
+      "Supply Qty": "1",
+    },
+    {
+      name: "Spare 2",
+      "Order Qty": "3",
+      "Supply Qty": "4",
+    },
+    {
+      name: "Spare 3",
+      "Order Qty": "5",
+      "Supply Qty": "6",
+    },
+  ] as const satisfies ({
+    [x in (typeof spareCols)[number]]: string;
+  } & { name: string })[];
+
+  const companies = [
+    {
+      name: "Company 1",
+      "Spare 1": {
+        selected: true,
+        total: 100,
+        unit: "D",
+      },
+      "Spare 2": {
+        selected: true,
+        total: 200,
+        unit: "W",
+      },
+      "Spare 3": {
+        selected: true,
+        total: 300,
+        unit: "M",
+      },
+    },
+  ] as const satisfies ({
+    [s in (typeof spares)[number]["name"]]: {
+      [x in (typeof companyCols)[number]]: string;
+    } & {
+      selected: boolean;
+      total: number;
+    };
+  } & {
+    name: string;
+  })[];
+
+  const aggregate = {
+    "Company 1": {
+      Discount: "10%",
+      "Amount Payable": "1000",
+      "Connect Date": "2022-01-01",
+      "Connect Port": "Port 1",
+      "Delivery Charge": "100",
+      Remark: "Remark 1",
+    },
+  } as const satisfies {
+    [company in (typeof companies)[number]["name"]]: {
+      [x in (typeof aggregateCols)[number]]: string;
+    };
+  };
+
   return (
     <QuoteCompareTable
       spareCols={spareCols as Mutable<typeof spareCols>}
       companyCols={companyCols as Mutable<typeof companyCols>}
       aggregateCols={aggregateCols as Mutable<typeof aggregateCols>}
+      spares={spares}
+      // @ts-ignore
+      companies={companies}
+      aggregate={aggregate}
     />
   );
 }
