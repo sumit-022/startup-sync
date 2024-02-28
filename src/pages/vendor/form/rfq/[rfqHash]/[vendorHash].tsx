6b9a7dbb5058bc9abd94c155e9dea28a53f2b916
sumit-022 <lcb2021016@iiitl.ac.in>
2024-02-29 03:04:06 +0530

    Changes

--- src/pages/vendor/form/rfq/[rfqHash]/[vendorHash].tsx
+++ src/pages/vendor/form/rfq/[rfqHash]/[vendorHash].tsx
@@ -14,8 +14,6 @@ import createAckPDF from "@/utils/create-rfq-ack";
 import downloadBlob from "@/utils/download-utils";
 import FormInputSelect from "@/components/atoms/input/select";
 import { useCurrency } from "@/context/CurrencyContext";
-import { useContext } from "react";
-import AuthContext from "@/context/AuthContext";
 
 type PageProps = {
   rfqs: any[];
@@ -83,7 +81,6 @@ const downloadAttachments = async (spare: any) => {
 
 export default function RfqHash(props: PageProps) {
   console.log(props.rfqs);
-  const { user } = useContext(AuthContext);
   const {
     register,
     control,
@@ -157,9 +154,7 @@ export default function RfqHash(props: PageProps) {
           },
         });
       }
-      const mailBody = `Dear Sir<br/><br/>Good Day!<br/><br/>We hereby acknowledge the receipt of the attached quotes. The provided information is currently under review, and we will revert to you with our confirmation with  a purchase order, if deemed suitable.<br/><br/>Regards<br/><br/><div style="display:flex;gap:20px"><img src="https://jobs.shinpoengineering.com/email.png" alt="Shinpo Engineering Pte Ltd" style="margin-right:10px"/><div><p style="font-weight: 700;color:#008ac9;font-size:20;margin:0">${user?.fullname}</p>Shinpo Engineering Pte. Ltd.<br/><br/><p style="margin:0;padding:0">${user?.designation}</p><p style="margin:0;padding:0">${user?.phone}</p><p style="margin:0;padding:0;color:#008ac9;">Email: purchase@shinpoengineering.com</p><p style="color:#008ac9;padding:0;margin:0;">1 Tuas South Avenue 6 #05-20 
-      The Westcom Singapore 637021</p>Tel: +65 65399007<br/>www.shinpoengineering.com
-      </div></div>`;
+      const mailBody = `Dear Sir<br/><br/>Good Day!<br/><br/>We hereby acknowledge the receipt of the attached quotes. The provided information is currently under review, and we will revert to you with our confirmation with  a purchase order, if deemed suitable.<br/><br/>Regards<br/><br/>`;
       const subject = `Acknowledgement of Quotes - ${props.job.jobCode}`;
       const pdf = createAckPDF({
         shipName: props.job.shipName,
